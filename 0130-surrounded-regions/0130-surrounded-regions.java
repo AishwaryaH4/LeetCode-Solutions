19f0934c1d4dd3ab46c9d42656a3bd52788a5bb6
Aishwarya Hegde <aishmhegde@gmail.com>
2026-04-10 15:23:03 +0530

    Time: 1 ms (99.92%), Space: 47.7 MB (62.99%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,13 +1,9 @@
 class Solution {
-
     public void solve(char[][] board) {
-        if (board == null || board.length == 0) return;
-
         int m = board.length;
         int n = board[0].length;
-
         for (int i = 0; i < m; i++) {
-            dfs(board, i, 0);       
+            dfs(board, i, 0);     
             dfs(board, i, n - 1);   
         }
 
@@ -15,14 +11,12 @@ class Solution {
             dfs(board, 0, j);      
             dfs(board, m - 1, j);   
         }
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X';  
-                } else if (board[i][j] == '#') {
-                    board[i][j] = 'O';   
-                }
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X'; 
+                else if (board[i][j] == 'T')
+                    board[i][j] = 'O';
             }
         }
     }
@@ -31,15 +25,14 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
 
-        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') {
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O')
             return;
-        }
 
-        board[i][j] = '#';
+        board[i][j] = 'T';
 
-        dfs(board, i - 1, j);
         dfs(board, i + 1, j);
-        dfs(board, i, j - 1);
+        dfs(board, i - 1, j);
         dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
     }
 }
