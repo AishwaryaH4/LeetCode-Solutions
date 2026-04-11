bbdb4ce30a0a0dd4951dc9df4b6f89f8b6691753
Aishwarya Hegde <aishmhegde@gmail.com>
2026-04-11 15:36:21 +0530

    Time: 41 ms (49.74%), Space: 45.8 MB (96.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,33 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
 
-        List<Integer> tails = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        int maxLen = 1;
 
-        for (int num : nums) {
-
-            int left = 0, right = tails.size();
-
-            // Binary search
-            while (left < right) {
-                int mid = (left + right) / 2;
-
-                if (tails.get(mid) < num) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-
-            // Replace or add
-            if (left == tails.size()) {
-                tails.add(num);
-            } else {
-                tails.set(left, num);
-            }
+            maxLen = Math.max(maxLen, dp[i]);
         }
-
-        return tails.size();
+        return maxLen;
     }
 }
