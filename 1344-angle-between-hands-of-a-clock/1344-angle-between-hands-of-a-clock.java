b542998b3f83882e689fe030487e9a505f33da66
Aishwarya Hegde <aishmhegde@gmail.com>
2026-06-18 21:22:14 +0530

    Time: 91 ms (2.56%), Space: 44.9 MB (43.59%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -1,9 +1,9 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
-        double minuteAngle = minutes * 6;
-        double hourAngle = (hour % 12) * 30 + minutes * 0.5;
+        final double minuteAngle = minutes * 6;
+        final double hourAngle = (hour % 12) * 30 + minutes * 0.5;
 
-        double diff = Math.abs(hourAngle - minuteAngle);
+        final double diff = Math.abs(hourAngle - minuteAngle);
         return Math.min(diff, 360 - diff);
     }
 }
